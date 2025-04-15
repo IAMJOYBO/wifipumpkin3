0f6793265deeb7a12bcdd899b395374b53c20802
IAMJOYBO <160575243+IAMJOYBO@users.noreply.github.com>
2025-04-16 05:52:08 +0800

    Update and rename ssh to push.sh

--- push.sh
+++ push.sh
@@ -1,10 +1,11 @@
 git config --global user.name "IAMJOYBO"
 git config --global user.email "13129311531@163.com"
 # ssh-keygen -t rsa -C '13129311531@163.com'
-git clone git@github.com:IAMJOYBO/UploadFile.git
+# git clone git@github.com:IAMJOYBO/UploadFile.git
 git status
-git lfs install
-git lfs track .
-git add .gitattributes .
-git commit -m "Upload Large File"
+# git lfs install
+# git lfs track .
+# git add .gitattributes .
+git add .
+git commit -m "Update"
 git push origin main
