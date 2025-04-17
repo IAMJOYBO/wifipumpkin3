314a1228bf43411322eefdf872dcb56513e9874d
IAMJOYBO <13129311531@163.com>
2025-04-17 08:10:12 +0800

    update

--- start.sh
+++ start.sh
@@ -2,13 +2,10 @@
 eval "$(conda shell.bash hook)"
 conda activate wifi
 wp3 --xpulp " \
-set ssid 401; \
+set ssid IAMJOYBO; \
 set interface wlan0; \
 set proxy captiveflask; \
-set captiveflask.WifiLogin true; \
-set captiveflask.force_redirect_https_connection true; \
-set captiveflask.force_redirect_to_url http://10.0.0.1; \
+set captiveflask.DarkLogin true; \
 ignore pydns_server; \
-set mode docker; \
 start
 "
